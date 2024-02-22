2c998473f0ca14adf2897a6d87376cfc72f17d6a
Trevor Benoit <Trevorb042@gmail.com>
2024-02-21 17:02:24 -0700

    complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function writeCards(names, eventName) {
   let thankYouMessages = []; 
   for (let i = 0; i < names.length; i++) { 
-    let message = `Thank you, ${names[i]}, for the wonderful ${eventName} surprise gift!`; 
+    let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`; 
     thankYouMessages.push(message); 
   }
   return thankYouMessages; 
